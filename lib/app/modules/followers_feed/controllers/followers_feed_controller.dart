d94080fcb72f7fe39a802e8782a58df1bbfb3df0
Thu Reain Htet Aung <thekings3079@gmail.com>
2026-01-03 00:21:06 +0700

    Fix

--- lib/app/modules/followers_feed/controllers/followers_feed_controller.dart
+++
@@ -1,5 +0,0 @@
-import '../../video_feed/controllers/video_feed_controller.dart';
-
-class FollowersFeedController extends VideoFeedController {
-  FollowersFeedController() : super(feedType: FeedType.followers);
-}
